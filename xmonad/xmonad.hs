3a482a91bbda9753d4442121f280581cf31d5efd
Martin Kirilov <49153369+mart9n@users.noreply.github.com>
2019-08-26 02:36:49 +0200

    Update xmonad.hs

--- xmonad/xmonad.hs
+++ xmonad/xmonad.hs
@@ -8,10 +8,12 @@ import XMonad.Util.EZConfig
 main = do
 xmproc <- spawnPipe "xmobar"
 xmonad $ defaultConfig
-  { modMask = mod4Mask -- use Super instead of Alt
+  { modMask = mod4Mask
   , terminal = "xterm"
   , manageHook = manageDocks <+> manageHook defaultConfig
   , layoutHook = avoidStruts $ layoutHook defaultConfig
   , logHook = dynamicLogWithPP $ defaultPP { ppOutput = hPutStrLn xmproc, ppOrder = \(ws:_:t:_) -> [ws,t] }
-  , borderWidth = 3
+  , borderWidth = 3 
   }
+  `additionalKeys`
+  [ ((mod4Mask, xK_p), spawn "exe=`dmenu_path | /home/martin/.cabal/bin/yeganesh -- -b -fn xft:Inconsolata:size=16` && eval \"exec $exe\"") ]
